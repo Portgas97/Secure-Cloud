554d3606f53005e269880c9df11c0d3d99d516bb
Francesco Venturini <fraventu97@Portgas.localdomain>
2023-03-03 16:31:46 +0100

    building the infrastructure

--- server/server.h
+++ server/server.h
@@ -1,14 +1,14 @@
-#include<stdio.h>
-#include<stdlib.h>
-#include<string.h>
-#include<unistd.h>
-#include<errno.h>
-#include<sys/types.h>
-#include<sys/socket.h>
-#include<sys/wait.h>
-#include<netinet/in.h>
-#include<arpa/inet.h>
-#include<signal.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <errno.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <sys/wait.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <signal.h>
 
 
 // parameters
