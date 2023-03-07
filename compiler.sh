c265ee71248016a1923f1e507aa0e959ea04a17d
Portgas97 <fraventu97@hotmail.it>
2023-03-07 23:30:29 +0100

    changed compiler.sh, modified gitignore to exclude executable files

--- compiler.sh
+++ compiler.sh
@@ -1,3 +1,3 @@
-g++ server/main.cpp common/utilities/ConnectionManager.cpp common/utilities/ServerConnectionManager.cpp common/utilities/CryptographyManager.cpp -o server.out -lcrypto -lpthread
+g++ server/*.cpp common/utilities/*.cpp -o server.out -lcrypto -lpthread
 
-g++ client/main.cpp common/utilities/ConnectionManager.cpp common/utilities/ClientConnectionManager.cpp common/utilities/CryptographyManager.cpp -o client.out -lcrypto -lpthread
+g++ client/*.cpp common/utilities/*.cpp -o client.out -lcrypto -lpthread
